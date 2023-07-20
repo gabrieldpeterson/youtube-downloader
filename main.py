8bc563fe3cfa3843a8d9fdb716e4183508fc801d
Gabriel Peterson <gabrieldpeterson@gmail.com>
2023-07-20 13:41:13 -0700

    Get code working

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import time
+from pytube import YouTube
+import os
+import sys
 
 
-# Press the green button in the gutter to run the script.
+def download_video(video_url):
+    yt = YouTube(video_url, use_oauth=True, allow_oauth_cache=True)
+    filename = yt.title.replace(' ', '_')
+    print(f'Downloading file: {yt.title}')
+
+    start_time = time.time()
+    path_to_download_folder = os.path.expanduser('~/Downloads')
+    yt.streams.get_highest_resolution().download(filename=os.path.join(path_to_download_folder, f'{filename}.mp4'))
+    end_time = time.time()
+
+    print(f'Download of {filename}.mp4 complete. {round(end_time - start_time, 2)} elapsed')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    if len(sys.argv) > 1 and 'youtube.com' in sys.argv[1]:
+        download_video(sys.argv[1])
+    else:
+        url = ''
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        while 'youtube.com' not in url:
+            url = input('Enter YouTube video URL: ')
+
+        download_video(url)
+
